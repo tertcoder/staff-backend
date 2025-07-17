37ca8e92991178a3b3ef53366a6b1bb8a7f34064
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2025-07-17 11:45:37 +0200

    Implement staff controller with CRUD operations

---
+++ controllers/staffController.js
@@ -0,0 +1,16 @@
+const Staff = require("../models/Staff");
+
+// GET ALL STAFF MEMBERS
+exports.getAllStaff = async (req, res) => {};
+
+// GET STAFF STATISTICS
+exports.getStaffStats = async (req, res) => {};
+
+// CREATE A NEW STAFF MEMBER
+exports.createStaff = async (req, res) => {};
+
+// UPDATE A STAFF MEMBER
+exports.updateStaff = async (req, res) => {};
+
+// DELETE A STAFF MEMBER
+exports.deleteStaff = async (req, res) => {};
